fb18b389a9ea749775d7f39f696ecf962efc0a96
Chris Hall <chall0311@gmail.com>
2024-10-14 14:33:09 -0400

    Create world class and setup new world.

---
+++ LightYearsEngine/src/framework/World.cpp
@@ -0,0 +1,33 @@
+#include "framework/World.h"
+#include "framework/Core.h"
+
+namespace ly {
+    World::World(Application* owningApp)
+        : mOwningApp(owningApp),
+        mBeganPlay(false)
+    {
+
+    }
+
+    void World::BeginPlayInternal() {
+        if (!mBeganPlay) {
+            mBeganPlay = true;
+            BeginPlay();
+        }
+    }
+
+    void World::TickInternal(float deltaTime) {
+        Tick(deltaTime);
+    }
+
+    World::~World() {
+    }
+
+    void World::BeginPlay() {
+        LOG("began play");
+    }
+
+    void World::Tick(float deltaTime) {
+        LOG("Tick frame rate %f", 1.f / deltaTime);
+    }
+}
