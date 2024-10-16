dc7a3e307e3d892dcfdfb00e36df3dd02ebde524
Chris Hall <chall0311@gmail.com>
2024-10-16 19:00:07 -0400

    Created spaceship class.

---
+++ LightYearsGame/src/spaceship/SpaceShip.cpp
@@ -0,0 +1,19 @@
+#include "spaceship/SpaceShip.h"
+
+namespace ly {
+    SpaceShip::SpaceShip(World* owningWorld, const std::string& texturePath)
+        : Actor(owningWorld, texturePath),
+        mVelocity()
+    {
+
+    }
+
+    void SpaceShip::Tick(float deltaTime) {
+        Actor::Tick(deltaTime);
+        AddActorLocationOffset(GetVelocity() * deltaTime);
+    }
+
+    void SpaceShip::SetVelocity(const sf::Vector2f& newVel) {
+        mVelocity = newVel;
+    }
+}
